4e6dd5e34ec0562a658b6cf2f2b1af964609a61f
Duygu Tabak <31600294+dtabak@users.noreply.github.com>
2024-04-03 22:19:52 -0700

    Create 13.sql

---
+++ Week 7/Movies/13.sql
@@ -0,0 +1,16 @@
+select distinct p.name from stars as s
+join people as p
+on s.person_id = p.id
+where s.movie_id in
+(
+    select movie_id from stars
+    where person_id in
+(
+    select id from people
+    where name = 'Kevin Bacon'
+    and birth = 1958
+)
+)
+and p.name != 'Kevin Bacon';
+
+
