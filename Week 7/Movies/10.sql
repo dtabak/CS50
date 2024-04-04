20d9d92b9d900300bf4de4959ec9eabd111919b9
Duygu Tabak <31600294+dtabak@users.noreply.github.com>
2024-04-03 22:19:02 -0700

    Create 10.sql

---
+++ Week 7/Movies/10.sql
@@ -0,0 +1,10 @@
+select name from people
+where id in
+(
+    select person_id from directors
+    where movie_id in
+    (
+        select movie_id from ratings
+        where rating >=9.0
+    )
+);
